a18be8a6556f0e62ea17a41e63035d6b91028885
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-01-21 00:06:24 -0800

    Add files via upload

---
+++ blanking.sh
@@ -0,0 +1,17 @@
+sudo vi /etc/systemd/system/enable-console-blanking.service
+
+[Unit]
+Description=Enable virtual console blanking
+
+[Service]
+Type=oneshot
+Environment=TERM=linux
+StandardOutput=tty
+TTYPath=/dev/console
+ExecStart=/usr/bin/setterm -blank 1
+
+[Install]
+WantedBy=multi-user.target
+
+sudo chmod 664 /etc/systemd/system/enable-console-blanking.service
+sudo systemctl enable enable-console-blanking.service
